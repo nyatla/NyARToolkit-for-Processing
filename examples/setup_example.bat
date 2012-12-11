898b6e01b8c35c47bcf50e5db60914226a61fde7
nyatla <nyatla@7cac0a50-4618-4814-88d0-24b83990f816>
2012-12-11 14:11:20 +0000

    git-svn-id: http://svn.osdn.jp/svnroot/nyartoolkit/nyar4psg/trunk@1119 7cac0a50-4618-4814-88d0-24b83990f816

--- examples/setup_example.bat
+++ examples/setup_example.bat
@@ -1,20 +1,55 @@
 cd %~dp0
 echo off
-echo This batch file copies library jar files to example code directories.
+echo For Processing 1.5.x.
+echo This batch file copies NyARToolkit library jar files for Processing 1.5.x to example code directories.
+set libpsg=..\library\NyAR4psg.jar
+set libnyar=..\library\NyARToolkit.jar
+
 pause
-copy ..\library\*.jar arPlusNyIdMarker\code\*
-copy ..\library\*.jar imagePickup\code\*
-copy ..\library\*.jar markerPlane\code\*
-copy ..\library\*.jar multiMarker\code\*
-copy ..\library\*.jar nonBeginSequence\code\*
-copy ..\library\*.jar rotation\code\*
-copy ..\library\*.jar simpleLite\code\*
-copy ..\library\*.jar simpleNyId\code\*
-copy ..\library\*.jar test\MultiMarkerTest\code\*
-copy ..\library\*.jar test\NyARBoardTest\code\*
-copy ..\library\*.jar test\SingleARTKMarkerTest\code\*
-copy ..\library\*.jar test\SingleNyIdMarkerTest\code\*
-copy ..\library\*.jar withOpenGL\code\*
-copy ..\library\*.jar withPicking\code\*
-copy ..\library\*.jar pngMarker\code\*
+del /q arPlusNyIdMarker\code\*.jar
+del /q imagePickup\code\*.jar
+del /q markerPlane\code\*.jar
+del /q multiMarker\code\*.jar
+del /q nonBeginSequence\code\*.jar
+del /q rotation\code\*.jar
+del /q simpleLite\code\*.jar
+del /q simpleNyId\code\*.jar
+del /q test\MultiMarkerTest\code\*.jar
+del /q test\NyARBoardTest\code\*.jar
+del /q test\SingleARTKMarkerTest\code\*.jar
+del /q test\SingleNyIdMarkerTest\code\*.jar
+del /q withOpenGL\code\*.jar
+del /q withPicking\code\*.jar
+del /q pngMarker\code\*.jar
+copy /y %libpsg% arPlusNyIdMarker\code\
+copy /y %libpsg% imagePickup\code\
+copy /y %libpsg% markerPlane\code\
+copy /y %libpsg% multiMarker\code\
+copy /y %libpsg% nonBeginSequence\code\
+copy /y %libpsg% rotation\code\
+copy /y %libpsg% simpleLite\code\
+copy /y %libpsg% simpleNyId\code\
+copy /y %libpsg% test\MultiMarkerTest\code\
+copy /y %libpsg% test\NyARBoardTest\code\
+copy /y %libpsg% test\SingleARTKMarkerTest\code\
+copy /y %libpsg% test\SingleNyIdMarkerTest\code\
+copy /y %libpsg% withOpenGL\code\
+copy /y %libpsg% withPicking\code\
+copy /y %libpsg% pngMarker\code\
+
+copy /y %libnyar% arPlusNyIdMarker\code\
+copy /y %libnyar% imagePickup\code\
+copy /y %libnyar% markerPlane\code\
+copy /y %libnyar% multiMarker\code\
+copy /y %libnyar% nonBeginSequence\code\
+copy /y %libnyar% rotation\code\
+copy /y %libnyar% simpleLite\code\
+copy /y %libnyar% simpleNyId\code\
+copy /y %libnyar% test\MultiMarkerTest\code\
+copy /y %libnyar% test\NyARBoardTest\code\
+copy /y %libnyar% test\SingleARTKMarkerTest\code\
+copy /y %libnyar% test\SingleNyIdMarkerTest\code\
+copy /y %libnyar% withOpenGL\code\
+copy /y %libnyar% withPicking\code\
+copy /y %libnyar% pngMarker\code\
 pause
